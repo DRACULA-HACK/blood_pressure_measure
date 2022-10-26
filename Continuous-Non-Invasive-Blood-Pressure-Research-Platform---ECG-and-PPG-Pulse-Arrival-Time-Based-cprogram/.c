ffbe5947ab4a327d5f258eb20626329304221e9c
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-10-26 22:30:49 +0530

    Create .c

---
+++ Continuous-Non-Invasive-Blood-Pressure-Research-Platform---ECG-and-PPG-Pulse-Arrival-Time-Based-cprogram/.c
@@ -0,0 +1,12 @@
+using System;
+namespace ConsoleApplication1
+{
+ public class Program
+ {
+ public static void Main(string[] args)
+ {
+ Console.WriteLine("By master-hack");
+ Console.Read();
+ }
+ }
+}
